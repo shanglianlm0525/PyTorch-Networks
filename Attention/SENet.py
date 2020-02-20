fe63f43582daf5ccf5ebd30c103ade05f47c13da
mingo <569793357@qq.com>
2020-02-20 16:26:46 +0800

    commit SENet

---
+++ Attention/SENet.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+
+def Conv1(in_planes, places, stride=2):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=in_planes,out_channels=places,kernel_size=7,stride=stride,padding=3, bias=False),
+        nn.BatchNorm2d(places),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    )
+
+class SE_Module(nn.Module):
+    def __init__(self, channel,ratio = 16):
+        super(SE_Module, self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+                nn.Linear(in_features=channel, out_features=channel // ratio),
+                nn.ReLU(inplace=True),
+                nn.Linear(in_features=channel // ratio, out_features=channel),
+                nn.Sigmoid()
+            )
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.squeeze(x).view(b, c)
+        z = self.excitation(y).view(b, c, 1, 1)
+        return x * z.expand_as(x)
+
+
+class SE_ResNetBlock(nn.Module):
+    def __init__(self,in_places,places, stride=1,downsampling=False, expansion = 4):
+        super(SE_ResNetBlock,self).__init__()
+        self.expansion = expansion
+        self.downsampling = downsampling
+
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(in_channels=in_places,out_channels=places,kernel_size=1,stride=1, bias=False),
+            nn.BatchNorm2d(places),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=places, out_channels=places, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(places),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=places, out_channels=places*self.expansion, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(places*self.expansion),
+        )
+
+        if self.downsampling:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels=in_places, out_channels=places*self.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(places*self.expansion)
+            )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        out = self.bottleneck(x)
+
+        if self.downsampling:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+        return out
+
+class SE_ResNet(nn.Module):
+    def __init__(self,blocks, num_classes=1000, expansion = 4):
+        super(SE_ResNet,self).__init__()
+        self.expansion = expansion
+
+        self.conv1 = Conv1(in_planes = 3, places= 64)
+
+        self.layer1 = self.make_layer(in_places = 64, places= 64, block=blocks[0], stride=1)
+        self.layer2 = self.make_layer(in_places = 256,places=128, block=blocks[1], stride=2)
+        self.layer3 = self.make_layer(in_places=512,places=256, block=blocks[2], stride=2)
+        self.layer4 = self.make_layer(in_places=1024,places=512, block=blocks[3], stride=2)
+
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(2048,num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def make_layer(self, in_places, places, block, stride):
+        layers = []
+        layers.append(SE_ResNetBlock(in_places, places,stride, downsampling =True))
+        for i in range(1, block):
+            layers.append(SE_ResNetBlock(places*self.expansion, places))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+def SE_ResNet50():
+    return SE_ResNet([3, 4, 6, 3])
+
+if __name__=='__main__':
+    model = SE_ResNet50()
+    print(model)
+
+    input = torch.randn(1, 3, 224, 224)
+    out = model(input)
+    print(out.shape)
+
+
+
+
+
